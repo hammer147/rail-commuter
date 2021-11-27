ddd720cdb3898f80fcd4d678fa518eeffa59c880
Ben Hamerlynck <ben.hamerlynck@telenet.be>
2021-11-27 17:33:33 +0100

    removed unnecessary link and meta tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,8 +7,6 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="manifest" href="/manifest.json" />
-          <link rel="apple-touch-icon" href="/icon.png"></link>
-          <meta name="theme-color" content="#fff" />
         </Head>
         <body>
           <Main />
